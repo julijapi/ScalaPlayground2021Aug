14fad8c9329d9fd6f4a8fbfc57192cdf182eefa5
ЮП <jp29115687@gmail.com>
2021-08-16 21:53:19 +0300

    jp renamed

--- ScalaPlayground2021Aug/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "ScalaPlayground2021Aug"
-
-version := "0.1"
-
-scalaVersion := "2.13.6"
--- ScalaPlayground2021Aug/src/main/scala/Variables.scala
+++
@@ -1,9 +0,0 @@
-object Variables extends App {
-  println(" Scala has two kind of variables, val and var")
-  val a =5 // stay constants
-  val b=6
-  var c = a + b // can change
-  println(a,b,c)
-  c=55
-  println(a,b,c)
-}
